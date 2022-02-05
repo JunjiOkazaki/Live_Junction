fff42ff2e3d9caf5eb0deb7a3fd9c3c10dd04635
岡嵜淳司 <jumac@okashinoMacBook-Pro.local>
2022-02-05 18:03:55 +0900

    セレクトボックス実装完了

--- app/javascript/packs/select_box_comp.js
+++ app/javascript/packs/select_box_comp.js
@@ -1,45 +1,42 @@
 $(document).on('turbolinks:load', function () {
-  return (function () {
-    //選択された親カテゴリのオプションから、data - children - pathの値を読み取る
-    let replaceChildrenOptions, replaceSelectOptions;
-    replaceSelectOptions = function ($select, results) {
-      $select.html($('<option>'));
-      return $.each(results, function () {
-        let option;
-        option = $('<option>').val(this.id).text(this.name);
-        return $select.append(option);
-      });
-    };
-    replaceChildrenOptions = function () {
-      //子カテゴリのセレクトボックスを取得する
-      let $selectChildren, childrenPath;
-      childrenPath = $(this).find('option:selected').data().childrenPath;
-      $selectChildren = $(this).closest('form').find('.select-children');
-      if (childrenPath != null) {
-        // childrenPathが存在する = 親カテゴリが選択されている場合、
-        // ajaxでサーバーに子カテゴリの一覧を問い合わせる
-        return $.ajax({
-          url: childrenPath,
-          dataType: "json",
-          success: function (results) {
-            //サーバーから受け取った子カテゴリの一覧でセレクトボックスを置き換える
-            return replaceSelectOptions($selectChildren, results);
-          },
-          error: function (XMLHttpRequest, textStatus, errorThrown) {
-            // 何らかのエラーが発生した場合
-            console.error("Error occurred in replaceChildrenOptions");
-            console.log(`XMLHttpRequest: ${XMLHttpRequest.status}`);
-            console.log(`textStatus: ${textStatus}`);
-            return console.log(`errorThrown: ${errorThrown}`);
+  for (let i = 1; i < 21; i++) {
+    (function (i) {
+      return (function () {
+        let replaceChildrenOptions, replaceSelectOptions;
+        replaceSelectOptions = function ($select, results) {
+          $select.html($('<option>'));
+          return $.each(results, function () {
+            let option;
+            option = $('<option>').val(this.id).text(this.name);
+            return $select.append(option);
+          });
+        };
+        replaceChildrenOptions = function () {
+          let $selectChildren, childrenPath;
+          childrenPath = $(this).find('option:selected').data().childrenPath;
+          $selectChildren = $(this).closest('form').find('.select-children' + i);
+          if (childrenPath != null) {
+            return $.ajax({
+              url: childrenPath,
+              dataType: "json",
+              success: function (results) {
+                return replaceSelectOptions($selectChildren, results);
+              },
+              error: function (XMLHttpRequest, textStatus, errorThrown) {
+                console.error("Error occurred in replaceChildrenOptions");
+                console.log(`XMLHttpRequest: ${XMLHttpRequest.status}`);
+                console.log(`textStatus: ${textStatus}`);
+                return console.log(`errorThrown: ${errorThrown}`);
+              }
+            });
+          } else {
+            return replaceSelectOptions($selectChildren, []);
           }
+        };
+        return $('.select-parent' + i).on({
+          'change': replaceChildrenOptions
         });
-      } else {
-        // 親カテゴリが未選択だったので、子カテゴリの選択肢をクリアする
-        return replaceSelectOptions($selectChildren, []);
-      }
-    };
-    return $('.select-parent').on({
-      'change': replaceChildrenOptions
-    });
-  })();
+      })();
+    })(i);
+  }
 });
