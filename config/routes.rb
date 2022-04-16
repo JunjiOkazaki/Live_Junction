4f08d6a8a2e317229ed30d8cb5fd507e9c616b25
岡嵜淳司 <jumac@okashinoMacBook-Pro.local>
2022-04-16 17:47:42 +0900

    gifアニメ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: { registrations: 'registrations' }
+
+  resources :users, only: :show
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root 'posts#index'
 
-  get '/users/:id', to: 'users#show', as: 'user'
+  #get '/users/:id', to: 'users#show', as: 'user'
 
   resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
